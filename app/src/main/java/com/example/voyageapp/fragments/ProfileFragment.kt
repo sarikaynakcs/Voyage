5e8141fefce8ba97a21263d743b78ad724525ecb
sarikaynakcs <sarikaynakcs@gmail.com>
2022-11-06 18:28:35 +0300

    06.11.2022 - Voyage App.v2

--- app/src/main/java/com/example/voyageapp/fragments/ProfileFragment.kt
+++ app/src/main/java/com/example/voyageapp/fragments/ProfileFragment.kt
@@ -1,19 +1,71 @@
 package com.example.voyageapp.fragments
 
+import android.annotation.SuppressLint
+import android.content.Intent
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.bumptech.glide.Glide
 import com.example.voyageapp.R
+import com.example.voyageapp.activities.ProfileUpdateActivity
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.database.DataSnapshot
+import com.google.firebase.database.DatabaseError
+import com.google.firebase.database.FirebaseDatabase
+import com.google.firebase.database.ValueEventListener
+import kotlinx.android.synthetic.main.fragment_profile.*
 
 
 class ProfileFragment : Fragment() {
 
+    private lateinit var firebaseAuth: FirebaseAuth
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
+        firebaseAuth = FirebaseAuth.getInstance()
+        loadUserInfo()
     }
+
+    private fun loadUserInfo() {
+        val ref = FirebaseDatabase.getInstance().getReference("Users")
+        ref.child(firebaseAuth.uid!!)
+            .addValueEventListener(object: ValueEventListener {
+                @SuppressLint("SetTextI18n")
+                override fun onDataChange(snapshot: DataSnapshot) {
+                    val email = "${snapshot.child("email").value}"
+                    val name = "${snapshot.child("name").value}"
+                    val surname = "${snapshot.child("surname").value}"
+                    val profileImage = "${snapshot.child("profileImage").value}"
+                    val timestamp = "${snapshot.child("timestamp").value}"
+                    val uid = "${snapshot.child("uid").value}"
+                    val userType = "${snapshot.child("userType").value}"
+
+                    // val formattedDate = Myapplication.formatTimeStamp(timestamp.toLong())
+
+                    fullNameId.text= name+" "+surname
+                    emailId.text = email
+
+                    try{
+                        Glide.with(this@ProfileFragment)
+                            .load(profileImage)
+                            .placeholder(R.drawable.ic_person_gray)
+                            .into(profileImageId)
+                    }
+                    catch (e: Exception){
+
+                    }
+
+                }
+
+                override fun onCancelled(error: DatabaseError) {
+                    TODO("Not yet implemented")
+                }
+            })
+    }
+
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -21,5 +73,14 @@ class ProfileFragment : Fragment() {
     ): View? {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_profile, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        editProfileId.setOnClickListener{
+            val intent = Intent (getActivity(), ProfileUpdateActivity::class.java)
+            getActivity()?.startActivity(intent)
+
+        }
     }
 }
